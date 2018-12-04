886f20afb919c18cdf88561738cfd4ab951d7d7d
Dyrk3 <boyer.411@buckeyemail.osu.edu>
2018-12-04 15:26:00 +0000

    Added/Sorted the rest of the items and added conq box

--- SmiteGenerator/index.py
+++
@@ -1,6 +0,0 @@
-with open("index.txt") as f:
-    content = f.readlines()
-    content = [x.strip() for x in content]
-    print(content)
-#for line in content:
-#    print("<option>" + line + "<option>")
