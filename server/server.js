9a3160d661634d961a297b66cd55a7158ba8f0a2
behm39 <behm.39@osu.edu>
2018-12-17 20:47:25 +0000

    changed port to 3000

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.listen(80, (event) => {
+app.listen(3000, (event) => {
     console.log('Listening on http://localhost');
 });
+
+
 
 app.use(express.static('../SmiteGenerator'));
